7b54a4d59544f0e87709497d888799283df75634
Sigve Hansen <sighan12@gmail.com>
2023-10-17 08:59:36 +0200

    feat: add analytics

--- app.vue
+++ app.vue
@@ -3,6 +3,13 @@ useHead({
   titleTemplate: (title) =>
     title ? `${title} | Sigve Hansen` : "Sigve Hansen",
   link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+  script: [
+    {
+      async: true,
+      src: "https://analytics.eu.umami.is/script.js",
+      "data-website-id": "25081cc6-3aeb-4ac5-ad2e-d8bf9bb57062",
+    },
+  ],
 });
 
 useSeoMeta({
