b3eeb0fb61ba14812665b4e1fd99117a2b8a63fe
Sigve Hansen <sighan12@gmail.com>
2024-01-19 13:28:34 +0100

    fix: update packages

---
+++ error.vue
@@ -0,0 +1,8 @@
+<template>
+  <main class="flex flex-col h-full p-4 md:p-8">
+    <AnimatedTitle title="Something_went_wrong." :should-animate="true" />
+    <NuxtLink to="/" class="px-4 py-2 border w-max border-current mt-6">
+      Go home
+    </NuxtLink>
+  </main>
+</template>
